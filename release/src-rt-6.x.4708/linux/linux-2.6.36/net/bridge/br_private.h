04b2205b8da83c124394de0c0e921de40af088ca
James Hilliard <james.hilliard1@gmail.com>
2013-11-06 07:47:24 -0600

    formatting differences

--- release/src-rt-6.x.4708/linux/linux-2.6.36/net/bridge/br_private.h
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/net/bridge/br_private.h
@@ -84,6 +84,7 @@ struct net_bridge_port_group {
 struct net_bridge_mdb_entry
 {
 	struct hlist_node		hlist[2];
+	struct hlist_node		mglist;
 	struct net_bridge		*br;
 	struct net_bridge_port_group	*ports;
 	struct rcu_head			rcu;
@@ -229,6 +230,7 @@ struct net_bridge
 	spinlock_t			multicast_lock;
 	struct net_bridge_mdb_htable	*mdb;
 	struct hlist_head		router_list;
+	struct hlist_head		mglist;
 
 	struct timer_list		multicast_router_timer;
 	struct timer_list		multicast_querier_timer;
