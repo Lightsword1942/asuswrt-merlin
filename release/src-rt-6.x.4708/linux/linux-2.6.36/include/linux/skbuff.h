795d8c681d002790da1ebf8173dc07d8d36f36af
James Hilliard <james.hilliard1@gmail.com>
2013-11-06 07:33:55 -0600

    fastpath changes

--- release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/skbuff.h
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/skbuff.h
@@ -333,6 +333,10 @@ struct sk_buff {
 	 * first. This is owned by whoever has the skb queued ATM.
 	 */
 	char			cb[48] __aligned(8);
+	char			fpath_cb[48] __aligned(8);    /* foxconn Bob added 02/06/2013 for cb is used by other kernel code */ 
+	
+	/* foxconn wklin added, 2010/06/15 @attach_dev */
+#define pp_bridge_indev(skb) (struct net_device **)(&(skb->cb[44]))
 
 	unsigned long		_skb_refdst;
 #if defined(CONFIG_XFRM) || defined(CTFMAP)
@@ -359,9 +363,7 @@ struct sk_buff {
 #endif /* BCMFA */
 
 	__u8			tcpf_smb:1,
-				tcpf_hdrbuf:1,
-				tcpf_nf:1;
-
+				tcpf_hdrbuf:1;
 	unsigned int		len,
 				data_len;
 	__u16			mac_len,
